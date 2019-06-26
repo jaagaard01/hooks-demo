6652a2051f7ae88c59897251f40687c38bfd1ed0
John Aagaard <j_aagaard@yahoo.com>
2019-06-25 22:24:53 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('World')
+
+  useEffect(() => {
+    document.title = `Hello, ${name}`; 
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello, {name}!</h1>
+      <button onClick={() => setName('John')}>
+        Click me to change the name
+      </button>
     </div>
   );
 }
